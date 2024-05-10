47ad25e592ad84b0b416bc07aafdb34ff0f11ac3
petelouise <programmer@petelouises.cloud>
2024-05-10 14:47:36 -0500

    Initial Commit

---
+++ Naggot/NaggotApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaggotApp.swift
+//  Naggot
+//
+//  Created by Pete Louise Brooks-Asclepiadoideae on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NaggotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
