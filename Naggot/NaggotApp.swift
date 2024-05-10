3ba032daabb994b0a15de0f4c98d52144500c4c8
petelouise <programmer@petelouises.cloud>
2024-05-10 15:04:38 -0500

    Refactor app delegate to initialize kiosk mode on launch

--- Naggot/NaggotApp.swift
+++ Naggot/NaggotApp.swift
@@ -9,16 +9,13 @@ import SwiftUI
 
 @main
 struct FullScreenNotificationApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .edgesIgnoringSafeArea(.all)
-                .onAppear {
-                    if let window = NSApp.windows.first {
-                        setupKioskMode(window: window)
-                    }
-                }
         }
         .windowStyle(HiddenTitleBarWindowStyle()) // Hide the title bar
         .windowToolbarStyle(UnifiedWindowToolbarStyle(showsTitle: false))
@@ -26,11 +23,21 @@ struct FullScreenNotificationApp: App {
             CommandGroup(replacing: .newItem, addition: {})
         }
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Setup the main application window
+        if let window = NSApp.windows.first {
+            setupKioskMode(window: window)
+        }
+    }
 
     private func setupKioskMode(window: NSWindow) {
         window.level = .statusBar
         window.collectionBehavior = [.canJoinAllSpaces, .fullScreenPrimary]
         window.styleMask.insert(.fullScreen)
+        window.toggleFullScreen(nil)  // Attempt to toggle full screen
         NSApp.activate(ignoringOtherApps: true)
     }
 }
