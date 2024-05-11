d94d5d89ce1e7a02e3c832996c949b3219765a67
petelouise <programmer@petelouises.cloud>
2024-05-10 21:39:39 -0500

    Refactor setupFullScreen method in NaggotApp.swift

--- Naggot/NaggotApp.swift
+++ Naggot/NaggotApp.swift
@@ -26,14 +26,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-private func setupFullScreen(window: NSWindow) {
-    window.setFrame(NSScreen.main!.frame, display: true, animate: true)
-    window.styleMask = [.fullSizeContentView, .borderless]
-    window.level = .screenSaver // Ensures the window is above most other windows, including the menu bar
-    window.isOpaque = true
-    window.backgroundColor = .clear
-    window.collectionBehavior = [.stationary, .canJoinAllSpaces, .fullScreenPrimary]
-    NSApp.presentationOptions = [.autoHideDock, .autoHideMenuBar, .disableProcessSwitching, .disableForceQuit, .disableSessionTermination] // Enforces a more strict kiosk mode
-}
+    private func setupFullScreen(window: NSWindow) {
+        window.setFrame(NSScreen.main!.frame, display: true, animate: true)
+        window.styleMask = [.fullSizeContentView, .borderless]
+        window.level = .screenSaver // Ensures the window is above most other windows, including the menu bar
+        window.isOpaque = true
+        window.backgroundColor = .clear
+        window.collectionBehavior = [.stationary, .canJoinAllSpaces, .fullScreenPrimary]
+        NSApp.presentationOptions = [.autoHideDock, .autoHideMenuBar, .disableProcessSwitching, .disableForceQuit, .disableSessionTermination] // Enforces a more strict kiosk mode
     }
 }
